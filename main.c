845efa88136f9dc55a5d2240701958ebac025393
zulhilmi <zulhilmiejannah13@gmail.com>
2025-12-06 16:48:00 +0800

    Added The Main function
    added main function for all operations
    
    Signed-off-by: zulhilmi <zulhilmiejannah13@gmail.com>

--- main.c
+++ main.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main()
+{
+    int a = 13;
+    int b = 10;
+    printf("Simple Calculator\n");
+
+    printf("a + b = %d\n", add_func(a,b));
+    printf("a - b = %d\n", sub_func(a,b));
+    printf("a * b = %d\n", mul_func(a,b));
+    printf("a / b = %d\n", div_func(a,b));
+
+    return 0;
+
+
+}
